7d02fb08a9ba5a0ab72d6fe9bb994fad391c550b
amerlon- <amerlon-@student.21-school.ru>
2019-04-09 21:27:11 +0300

    fixed seg fault in disasm

--- Disassembler/src/main.c
+++ Disassembler/src/main.c
@@ -6,35 +6,46 @@
 /*   By: amerlon- <amerlon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/21 18:16:49 by amerlon-          #+#    #+#             */
-/*   Updated: 2019/03/28 13:22:11 by amerlon-         ###   ########.fr       */
+/*   Updated: 2019/04/09 21:24:53 by amerlon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "disassemble.h"
 
-int	main(int argc, char **argv)
+void	start(int fd, char *argv)
 {
-	int		fd;
 	int		fd_out;
 	int		i;
 	char	*name;
 
+	name = ft_strnew(ft_strlen(argv) - 2);
+	ft_memmove(name, argv, ft_strlen(argv) - 2);
+	i = ft_strrchr(name, '.') - name;
+	name[i + 1] = 's';
+	name[i + 2] = '\0';
+	fd_out = open(name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	lseek(fd, 0, SEEK_SET);
+	disassemble(fd, fd_out);
+	ft_putstr("Writing output program to ");
+	ft_putendl(name);
+	close(fd);
+	close(fd_out);
+	ft_strdel(&name);
+}
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+
 	if (argc > 1)
 	{
 		fd = open(argv[1], O_RDONLY);
-		name = ft_strnew(ft_strlen(argv[1]) - 2);
-		ft_memmove(name, argv[1], ft_strlen(argv[1]) - 2);
-		i = ft_strrchr(name, '.') - name;
-		name[i + 1] = 's';
-		name[i + 2] = '\0';
-		fd_out = open(name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		lseek(fd, 0, SEEK_SET);
-		disassemble(fd, fd_out);
-		ft_putstr("Writing output program to ");
-		ft_putendl(name);
-		close(fd);
-		close(fd_out);
-		ft_strdel(&name);
+		if (fd < 0)
+		{
+			ft_putstr("File error\n");
+			return (0);
+		}
+		start(fd, argv[1]);
 	}
 	else
 		ft_putstr("usage: file.cor\n");

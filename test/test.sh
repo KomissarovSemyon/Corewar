c95f5d8842dce608275bc6f01c60f800fda3bb57
amerlon- <amerlon-@student.21-school.ru>
2019-04-09 17:26:51 +0300

    added disassembler tests

--- test/test.sh
+++ test/test.sh
@@ -80,7 +80,6 @@ test_asm_valgrind()
 {
 	CHAMPS=$(find ./champs -name *.s)
 	COUNT=0
-	CORRECT=$(echo "definitely lost: 0 bytes in 0 blocks\nindirectly lost: 0 bytes in 0 blocks")
 	CORRECT=$(echo "0\n0")
 	for CHAMP in $CHAMPS
 	do
@@ -92,16 +91,72 @@ test_asm_valgrind()
 		else
 			printf "%s: \e[1;32mOK\e[0m\n" "$CHAMP"
 		fi
+		rm -rf ${CHAMP%.s}.cor
 	done
 	echo $COUNT
 }
 
-if [[ $# -eq 0 ]] ; then
-	echo 'options: asm, asm_valgrind, vm, disasm'
+test_disasm() {
+	CHAMPS=$(find . -name "*.s")
+
+	for CHAMP in $CHAMPS
+	do
+		OUTPUT=$(./asm_original $CHAMP | grep -i 'writing')
+		if [ "$OUTPUT" ]
+		then
+			mv $CHAMP ${CHAMP%.s}_tmp.s
+			hexdump -Cv ${CHAMP%.s}.cor > tmp_original
+			./disassemble ${CHAMP%.s}.cor > /dev/null
+			./asm $CHAMP > /dev/null
+			hexdump -Cv ${CHAMP%.s}.cor > tmp_da
+			DIFF=$(diff tmp_original tmp_da)
+			rm -rf tmp_original tmp_da
+			if [ -z "$DIFF" ]
+			then
+				printf "%s: \e[1;32mOK\e[0m\n" "$CHAMP"
+			else
+				printf "%s: \e[1;31mKO\e[0m\n" "$CHAMP"
+				echo "$DIFF"
+			fi
+			mv ${CHAMP%.s}_tmp.s $CHAMP
+		fi
+		rm -rf ${CHAMP%.s}.cor
+	done 2> /dev/null
+}
+
+test_disasm_valgrind()
+{
+	CHAMPS=$(find . -name *.s)
+	COUNT=0
+	CORRECT=$(echo "0\n0")
+	for CHAMP in $CHAMPS
+	do
+		OUTPUT=$(./asm_original $CHAMP | grep -i 'writing')
+		if [ "$OUTPUT" ]
+		then
+			cp $CHAMP ${CHAMP%.s}_tmp.s
+			OUTPUT=$(valgrind --leak-check=full --show-leak-kinds=all ./disassemble ${CHAMP%.s}.cor 2>&1 | grep -E "definitely|indirectly" | awk '{ print $4 }')
+			if test "$OUTPUT" != "$CORRECT"
+			then
+				printf "%s: \e[1;31mKO\e[0m\n" "$CHAMP"
+				echo "$OUTPUT"
+				COUNT=$(($COUNT + 1))
+			else
+				printf "%s: \e[1;32mOK\e[0m\n" "$CHAMP"
+			fi
+			rm -rf ${CHAMP%.s}.cor
+			mv ${CHAMP%.s}_tmp.s $CHAMP
+		fi
+	done
+	echo $COUNT
+}
+
+if [[ $# -eq 0 ]]; then
+	echo 'options: asm, asm_valgrind, vm, disasm, disasm_valgrind'
 	exit 0
 fi
 
-if [ $1 = 'asm' ] ; then
+if [ $1 = 'asm' ]; then
 	make -C ../Assembler re > /dev/null
 	cp ../Assembler/asm . > /dev/null
 	make -C ../Assembler fclean > /dev/null
@@ -111,10 +166,26 @@ if [ $1 = 'asm' ] ; then
 	rm -rf asm > /dev/null
 fi
 
-if [ $1 = 'asm_valgrind' ] ; then
+if [ $1 = 'asm_valgrind' ]; then
 	make -C ../Assembler re > /dev/null
 	cp ../Assembler/asm . > /dev/null
 	make -C ../Assembler fclean > /dev/null
 	test_asm_valgrind
 	rm -rf asm > /dev/null
+fi
+
+if [ $1 = 'disasm' ] ; then
+	make -C ../Disassembler re > /dev/null
+	cp ../Disassembler/disassemble . > /dev/null
+	make -C ../Disassembler fclean > /dev/null
+	test_disasm
+	rm -rf disassemble > /dev/null
+fi
+
+if [ $1 = 'disasm_valgrind' ] ; then
+	make -C ../Disassembler re > /dev/null
+	cp ../Disassembler/disassemble . > /dev/null
+	make -C ../Disassembler fclean > /dev/null
+	test_disasm_valgrind
+	rm -rf disassemble > /dev/null
 fi
